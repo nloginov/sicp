207080c6190011c67bfa440baf78a62bb826dd86
nick <kolyanlab@yandex.ru>
2013-01-06 10:53:56 +0600

    1.19 exercise

---
+++ 1.19.scm
@@ -0,0 +1,28 @@
+(load "assert.scm")
+
+(define (fib n)
+  (fib-iter 1 0 0 1 n))
+
+(define (fib-iter a b p q count)
+  (cond ((= count 0) b)
+        ((even? count)
+         (fib-iter a
+                   b
+                   (+ (* q q) (* p p))
+                   (+ (* q q) (* 2 p q))
+                   (/ count 2)))
+        (else (fib-iter (+ (* b q) (* a q) (* a p))
+                        (+ (* b p) (* a q))
+                        p
+                        q
+                        (- count 1)))))
+
+
+(assert-equal 0 (fib 0))
+(assert-equal 1 (fib 1))
+(assert-equal 1 (fib 2))
+(assert-equal 2 (fib 3))
+(assert-equal 3 (fib 4))
+(assert-equal 55 (fib 10))
+(assert-equal 39088169 (fib 38))
+
