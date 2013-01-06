05f2478772ca1542f3b10997f3088beac9944d00
nick <kolyanlab@yandex.ru>
2013-01-06 16:36:34 +0600

    fix indention

--- assert.scm
+++ assert.scm
@@ -2,17 +2,17 @@
   (if (= expected actual)
     (display "Test pass\n")
     (display "Test failed\n")
-  ))
+    ))
 
 (define (assert-false predicate)
-	(if (not predicate)
-		(display "Test pass\n")
-		(display "Test failed\n")
-	))
+  (if (not predicate)
+    (display "Test pass\n")
+    (display "Test failed\n")
+    ))
 
 (define (assert-close expected actual)
   (if (< (abs (- expected actual)) 0.0001)
     (display "Test pass\n")
     (display "Test failed\n")
-  ))
+    ))
 
